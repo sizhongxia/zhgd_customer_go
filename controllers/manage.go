d5accb5f354b36b74eb9b18c4f82c58460253861
sizhongxia <sizhongxia@haijupai.com>
2018-11-27 10:20:10 +0800

    Update controllers/manage.go
    移除无效代码

--- controllers/manage.go
+++ controllers/manage.go
@@ -12,21 +12,6 @@ func (c *ManageController) Index() {
 	c.TplName = "manage/index.tpl"
 }
 
-// func (c *ManageController) Tpl() {
-// 	// time.Sleep(time.Duration(3)*time.Second)
-// 	c.TplName = "manage/tpl.tpl"
-// }
-
-// func (c *ManageController) Project() {
-// 	c.TplName = "manage/project.tpl"
-// }
-// func (c *ManageController) Account() {
-// 	c.TplName = "manage/account.tpl"
-// }
 func (c *ManageController) Memorabilia() {
 	c.TplName = "manage/memorabilia.tpl"
 }
-
-// func (c *ManageController) Personnel() {
-// 	c.TplName = "manage/personnel.tpl"
-// }
